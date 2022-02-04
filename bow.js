6e458c4a288c43eaffd9ebc7717640c822ee52b1
nnrv <87524839+nnrv@users.noreply.github.com>
2022-02-04 15:47:57 +0530

    Add files via upload

---
+++ bow.js
@@ -0,0 +1,14 @@
+class Bow {
+ constructor(x,y,width,height){
+     const options = {
+         isStatic: true
+     };
+
+     this.body = Matter.Bodies.rectangle(x,y,width,height)
+
+ }
+
+
+
+
+}
